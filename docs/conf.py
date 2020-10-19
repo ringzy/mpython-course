1d406036d4b82788ddff068ce27d7b14ed9bd7ff
ringzy <zyring@163.com>
2020-10-19 22:32:48 +0800

    change title

--- docs/conf.py
+++ docs/conf.py
@@ -52,5 +52,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_title = '掌控板创意编程'
 
 master_doc = 'index'
